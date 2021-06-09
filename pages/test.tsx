a3e8650a506ddb2ab1f1a003c5e8699fdc3f5ab2
j-94 <jasedgwic@gmail.com>
2021-06-09 16:23:58 +0100

    create servicecard+cardgrid

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import Image from "next/image";
 import path from "path";
 import * as fs from "fs";
 import PageHead from "components/head";
@@ -10,12 +11,13 @@ import {
   Button,
   chakra,
   Stack,
+  SimpleGrid,
+  GridItem,
   useColorModeValue,
-  Link,
-  Flex,
-  Center,
-  Heading,
   ControlBox,
+  Heading,
+  Center,
+  Link,
   ButtonGroup,
 } from "@chakra-ui/react";
 import { FaArrowRight } from "react-icons/fa";
@@ -23,86 +25,44 @@ import { DiGithubBadge } from "react-icons/di";
 import NextLink from "next/link";
 import Feature1 from "components/landing/feature1";
 import Feature2 from "components/landing/feature2";
-
-import Form from "components/forms/hybridform";
-import ChocoForm from "components/forms/chocoform";
-import { Vertical } from "components/forms/steps";
-import {
-  CheckboxContainer,
-  CheckboxControl,
-  CheckboxSingleControl,
-  InputControl,
-  NumberInputControl,
-  PercentComplete,
-  PinInputControl,
-  RadioGroupControl,
-  ResetButton,
-  SelectControl,
-  SliderControl,
-  SubmitButton,
-  SwitchControl,
-  TextareaControl,
-} from "formik-chakra-ui";
-import servicecard from "components/forms/servicecard";
-import Image from "next/image";
+import OpenSource from "components/landing/open-source";
+import Sponsors from "components/landing/sponsors";
+import ServiceCard from "components/landing/servicecard";
 
 const Home: NextPage = (props: any) => {
   return (
     <SiteLayout>
       <PageHead title="Build in Surrey" />
-      <ButtonGroup>
-        <SubmitButton>Submit</SubmitButton>
-        <ResetButton>Reset</ResetButton>
-      </ButtonGroup>
-      <Center py={6}>
-        <Box
-          as="button"
-          transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
-          _hover={{ bg: "#ebedf0" }}
-          _active={{
-            bg: "#dddfe2",
-            transform: "scale(0.98)",
-            borderColor: "#bec3c9",
-          }}
-          _focus={{
-            boxShadow:
-              "0 0 1px 2px rgba(88, 144, 255, .75), 0 1px 1px rgba(0, 0, 0, .15)",
-          }}
-          maxW={"445px"}
-          w={"full"}
-          bg={useColorModeValue("white", "gray.900")}
-          boxShadow={"2xl"}
-          rounded={"md"}
-          p={6}
-          overflow={"hidden"}
-        >
-          <Box
-            h={"210px"}
-            bg={"gray.100"}
-            mt={-6}
-            mx={-6}
-            mb={6}
-            pos={"relative"}
-          >
-            <Image
-              src={
-                "https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-              }
-              layout={"fill"}
-            />
-          </Box>
-          <Stack>
-            <Heading
-              color={useColorModeValue("gray.700", "white")}
-              fontSize={"2xl"}
-              fontFamily={"body"}
-            >
-              Rennovation
-            </Heading>
-          </Stack>
+      <SimpleGrid
+        display={{ base: "initial", md: "grid" }}
+        columns={{ md: 2 }}
+        spacing={{ md: 6 }}
+      >
+        <Box pt={5}>
+          <GridItem colSpan={{ md: 1 }}>
+            <SimpleGrid columns={6} spacing={5}>
+              <Box as={GridItem} colSpan={[6, 3]}>
+                <ServiceCard />
+              </Box>
+              <Box as={GridItem} colSpan={[6, 3]}>
+                <ServiceCard />
+              </Box>
+            </SimpleGrid>
+          </GridItem>
+        </Box>{" "}
+        <Box pt={5}>
+          <GridItem colSpan={{ md: 1 }}>
+            <SimpleGrid columns={6} spacing={5}>
+              <Box as={GridItem} colSpan={[6, 3]}>
+                <ServiceCard />
+              </Box>
+              <Box as={GridItem} colSpan={[6, 3]}>
+                <ServiceCard />
+              </Box>
+            </SimpleGrid>
+          </GridItem>
         </Box>
-        <ControlBox />
-      </Center>
+      </SimpleGrid>
     </SiteLayout>
   );
 };
